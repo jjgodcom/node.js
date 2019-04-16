29620a8ffab9206a81f04e5eadc58cc44616488e
jjgodcom_company <jjgodcom1@gmail.com>
2019-04-16 14:03:58 +0900

    15.JavaScript-비교연산자

---
+++ nodejs/syntax/comparison.js
@@ -0,0 +1,8 @@
+console.log(1+1);
+console.log(2);
+console.log(1==1); // 같으면 true 다르면 flase
+console.log(1==2); // 다르면 false
+console.log(1>2); // false
+console.log(1<2); //false
+console.log(1===1); //true
+console.log(1===2); //false
