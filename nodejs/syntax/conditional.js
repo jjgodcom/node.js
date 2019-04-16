0b13687e02647ba8557ca9b120ff39e0c132aa09
jjgodcom_company <jjgodcom1@gmail.com>
2019-04-16 15:03:26 +0900

    18.NodeJS-콘솔에서의 입력값

--- nodejs/syntax/conditional.js
+++ nodejs/syntax/conditional.js
@@ -1,11 +1,8 @@
+var args = process.argv;
+console.log(args[2]);
 console.log('a');
 console.log('b');
-if(true){
-    console.log('c1');
-}else{
-    console.log('c2');
-}
-if(false){
+if(args[2] === "1"){
     console.log('c1');
 }else{
     console.log('c2');
