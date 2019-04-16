df661497b0017148f6e7e7479e769506764f2ee3
jjgodcom_company <jjgodcom1@gmail.com>
2019-04-16 14:51:56 +0900

    17.JavaScript-조건문

---
+++ nodejs/syntax/conditional.js
@@ -0,0 +1,13 @@
+console.log('a');
+console.log('b');
+if(true){
+    console.log('c1');
+}else{
+    console.log('c2');
+}
+if(false){
+    console.log('c1');
+}else{
+    console.log('c2');
+}
+console.log('d');
