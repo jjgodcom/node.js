76b8ac3adee02f13c28579449d6212df0f511fee
jjgodcom_company <jjgodcom1@gmail.com>
2019-04-17 15:40:10 +0900

    21. JavaScript-배열

---
+++ nodejs/syntax/array.js
@@ -0,0 +1,8 @@
+var arr = ["A","B","C","D"]; //배열을 선언
+console.log(arr[1]); //배열을 읽는다 , 배열은 0부터 시작함
+console.log(arr[3]);
+arr[2] = 3; // 3번째 배열을 3으로 바꾼다.
+console.log(arr);
+console.log(arr.length);// 개수를 셀때는 1부터 시작
+arr.push("E"); // 배열에 E를 추가함
+console.log(arr);
