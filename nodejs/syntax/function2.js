50097bd8a8caa01d9bdae34db3e8b32deab8a562
jjgodcom_company <jjgodcom1@gmail.com>
2019-04-18 11:11:27 +0900

    Node.js - 25.2.JavaScript 함수의 입력

--- nodejs/syntax/function2.js
+++ nodejs/syntax/function2.js
@@ -6,3 +6,15 @@ function f123(){
 f123();
 console.log("--");
 f123();
+console.log("--");
+
+ // round() 라는 함수는 반올림해줌
+console.log(Math.round(1.6)); //2
+console.log(Math.round(1.4)); //1
+console.log("--");
+
+function sum(first){ // parameter
+    console.log(first);
+  }
+   
+  sum(2,4); // argument
