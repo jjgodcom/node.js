cb476ca5679fdba6137db53d9f18eaa74f96ef19
jjgodcom_company <jjgodcom1@gmail.com>
2019-04-22 11:14:57 +0900

    39.JavaScript 객체의 형식

---
+++ nodejs/syntax/object.js
@@ -0,0 +1,11 @@
+//배열은 순서대로 넣으면된다
+var members = ['egg','tag','g1'];
+console.log(members[1]); //tag
+
+//객체는 각각의 데이터에 고유의 이름을 준다
+var roles = {
+    'pro':'me',
+    'des':'k88',
+    'manage':'su'
+};
+console.log(roles.des); // k88
