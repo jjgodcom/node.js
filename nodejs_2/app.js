2f05ad276fd1de5d29e7562c07f9ca720cd8385d
jjgodcom_company <jjgodcom1@gmail.com>
2019-04-24 16:06:56 +0900

    2강 - 호출

--- nodejs_2/app.js
+++ nodejs_2/app.js
@@ -1,1 +1,20 @@
-console.log("hello");
+var dog = {
+    name:"jjgodcom",
+    age:7
+}
+
+console.log(dog);
+
+function sum(a,b){
+    return a+b;
+}
+
+console.log(sum(1,3));
+
+var print = function(){
+    console.log("print");
+}
+
+print();
+
+setTimeout(print, 7000); //7초 후에 print를 실행함
