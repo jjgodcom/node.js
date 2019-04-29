b06343c74c0e22e6016ff3c786e4d349dea2a0c7
jjgodcom_company <jjgodcom1@gmail.com>
2019-04-29 09:53:29 +0900

    Express 설치

--- nodejs_2/server.js
+++ nodejs_2/server.js
@@ -1,22 +1,17 @@
-var http = require("http"); //http 모듈을 불러온다
-var fs = require("fs"); // 파일 시스템 모듈을 불러온다. 파일을 읽고쓰고지울수있는 중요한 코어 모듈이다
+var connect = require("connect");
+var http = require("http");
 
-// 404에러(웹페이지 없을때)
-function send404Response(response){
-    response.writeHead(404,{"Content-type":"text/plain"});
-    response.write("404 Error: Opps!!!!");
-    response.end();
+var app = connect(); 
+
+function about(req,res){
+    console.log('사용자가 about 페이지를 요청함');
 }
-// 사용자 요구에 응답 처리
-function onRequest(request,response){
-    if(request.method == 'GET' && request.url == '/'){
-        response.writeHead(200,{"Content-type":"text/html"});
-        fs.createReadStream("./index.html").pipe(response);
-    }else{
-        //없는 파일을 찾는경우
-        send404Response(response);
-    }
+function email(req,res){
+    console.log('사용자가 email 페이지를 요청함');
 }
 
-http.createServer(onRequest).listen(8888);
-console.log("서버가 실행 됨");
+app.use('/about', about);
+app.use('/email', email);
+
+http.createServer(app).listen(8888);
+console.log("서버가 작동 중");
